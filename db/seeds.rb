25328dba08ab06c16b0a61945c7410107d263e8d
Erin Van Brunt <erinmontybruce@gmail.com>
2022-04-21 11:35:28 -0700

    added new creatures, but some imgs aren't working

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,21 @@ user1 = User.create(username: "Jedi")
 user2 = User.create(username: "Light")
 
 puts "Creating creatures..."
-
-c1 = Creature.create(creature_type: "Porg", film: "Star Wars: The Last Jedi", image_url: "https://static.wikia.nocookie.net/disney/images/a/aa/Porg.jpg/revision/latest/scale-to-width-down/1030?cb=20180103040758")
-c2 = Creature.create(creature_type: "Jawas", film: "Star Wars: The Rise of Skywalker", image_url: "https://static.wikia.nocookie.net/disney/images/0/02/Jawa.jpg/revision/latest/scale-to-width-down/1032?cb=20191024002057")
-c3 = Creature.create(creature_type: "Ewoks", film: "Star Wars: The Rise of Skywalker", image_url: "https://static.wikia.nocookie.net/disney/images/c/c2/Ewok.jpg/revision/latest/scale-to-width-down/1032?cb=20191024001658")
-c4 = Creature.create(creature_type: "Hutt", film: "Star Wars: Return of the Jedi", image_url: "https://static.wikia.nocookie.net/disney/images/4/44/Jabba.png/revision/latest/scale-to-width-down/1031?cb=20130430195617")
-c5 = Creature.create(creature_type: "Bantha", film: "Star Wars: The Phantom Menace", image_url:"https://static.wikia.nocookie.net/disney/images/a/a2/Bantha-ST.jpg/revision/latest?cb=20130604032107")
+c1 = Creature.create(creature_type: "Porg", film: "Star Wars: The Last Jedi", image_url: "https://cdn.mos.cms.futurecdn.net/X5qTLtgXw7XBU9XUQjszE3.jpg")
+c2 = Creature.create(creature_type: "Jawas", film: "Star Wars: The Rise of Skywalker", image_url: "https://i.pinimg.com/originals/f9/0b/f7/f90bf7d05227a12de42df31698b4c24a.jpg")
+c3 = Creature.create(creature_type: "Ewoks", film: "Star Wars: The Rise of Skywalker", image_url: "https://i.pinimg.com/736x/4d/28/b6/4d28b6a6b35c6a5d32abaa602c40cdeb--warwick-davis-finn-star-wars.jpg")
+c4 = Creature.create(creature_type: "Hutt", film: "Star Wars: Return of the Jedi", image_url: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/star-wars-episode-7/8/83/Jabba-the-hut.jpg")
+c5 = Creature.create(creature_type: "Bantha", film: "Star Wars: The Phantom Menace", image_url:"https://fictionhorizon.com/wp-content/uploads/2022/01/What-Do-Banthas-Eat-In-Star-Wars-What-Is-A-Bantha-Fodder-04.jpg")
+c6 = Creature.create(creature_type: "Dewbacks", film: "Star Wars: A New Hope", image_url:"https://www.gamespot.com/a/uploads/scale_medium/1561/15615114/2964179-dewbacks.jpg")
+c7 = Creature.create(creature_type: "Wookiees", film: "Star Wars: Revenge of the Sith", image_url:"https://i.pinimg.com/474x/7b/86/91/7b869162e01c276b286788d9ab6514f2.jpg")
+c8 = Creature.create(creature_type: "Krayt Dragon", film: "Star Wars: A New Hope", image_url:"https://boundingintocomics.com/wp-content/uploads/2020/10/2020.10.31-04.30-boundingintocomics-5f9d9135ccf60.jpg")
+c9 = Creature.create(creature_type: "Kel Dors", film: "Star Wars: Attack of the Clones", image_url:"https://www.denofgeek.com/wp-content/uploads/2021/08/plo-koon-mandalorian-dailies-render-disney.jpg?fit=1200%2C680")
+c10 = Creature.create(creature_type: "Yoda", film: "Star Wars: Attack of the Clones", image_url:"https://cdn.ndtv.com/tech/images/gadgets/yoda_dagobah.jpg?")
+c11 = Creature.create(creature_type: "Vulptex", film: "Star Wars: The Last Jedi", image_url:"https://insidethemagic.net/wp-content/uploads/2017/11/CrystalFox-800x400.jpg")
+c12 = Creature.create(creature_type: "Sarlacc", film: "Star Wars: Return of the Jedi", image_url:"https://www.jornalciencia.com/wp-content/uploads/2017/01/sarlacc_01-1000x500.jpg")
+c13 = Creature.create(creature_type: "Tusken Raiders", film: "Star Wars: A New Hope", image_url:"https://fictionhorizon.com/wp-content/uploads/2022/01/tusken-raiders-1024x577.jpg")
+c14 = Creature.create(creature_type: "Geonosian", film: "Star Wars: Revenge of the Sith", image_url:"https://wiki.darkjedibrotherhood.com/images/5/53/Xaezhul1.jpg")
+c15 = Creature.create(creature_type: "Vexis", film: "Star Wars: The Rise of Skywalker", image_url:"https://i.pinimg.com/564x/3a/db/c5/3adbc5bfa1a38f1d9a77f250a2fc1f06.jpg")
 
 puts "Creating pets..."
 Pet.create(user_id: user1.id, creature_id: c1.id)
