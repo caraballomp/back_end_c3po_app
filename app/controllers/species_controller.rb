8082715fd0cee33db403f6bd714552abf2478608
Erin Van Brunt <erinmontybruce@gmail.com>
2022-04-22 08:58:25 -0700

    DONE.

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,27 +1,30 @@
 class SpeciesController < ApplicationController
   get '/species' do
-    Species.where(user_id: @user.id).to_json
+    # Species.where(id: @id).to_json
+
+    Species.all.to_json
   end
 
   post '/species' do
-    # return { message: 'Invalid session' }.to_json
     @species =
       Species.create(
         species_name: params[:species_name],
         description: params[:description],
       ).to_json
     @species.save
-    #   return @species.to_json
-    # else
-    #   'Error: Species not created'
-    # end
   end
 
-  delete '/species' do
-    return { message: 'Invalid session' }.to_json
+  delete '/species/:id' do
+    # return { message: 'Invalid session' }.to_json
 
-    @species = Species.find_by(id: params[:id])
-    @species.delete
-    return { message: 'Species Deleted' }.to_json
+    # @species = Species.find_by(id: params[:id])
+    # @species.delete
+    # return { message: 'Species Deleted' }.to_json
+    Species.find_by_id(params[:id]).destroy
   end
+
+  # delete '/pet/:id' do
+  #   Pet.find_by_id(params[:id]).destroy
+
+  # end
 end
